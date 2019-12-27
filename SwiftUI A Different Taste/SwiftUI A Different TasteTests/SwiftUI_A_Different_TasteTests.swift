7ef5f4883130c31064970f84db9eb87e1c52f81b
Farooq Rasheed <farooq1045@gmail.com>
2019-12-27 20:35:33 +0500

    project init

---
+++ SwiftUI A Different Taste/SwiftUI A Different TasteTests/SwiftUI_A_Different_TasteTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_A_Different_TasteTests.swift
+//  SwiftUI A Different TasteTests
+//
+//  Created by Farooq Rasheed on 27/12/2019.
+//  Copyright © 2019 Farooq Rasheed. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_A_Different_Taste
+
+class SwiftUI_A_Different_TasteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ SwiftUI A Different Taste/SwiftUI A Different TasteUITests/SwiftUI_A_Different_TasteUITests.swift
@@ -0,0 +1,43 @@
+//
+//  SwiftUI_A_Different_TasteUITests.swift
+//  SwiftUI A Different TasteUITests
+//
+//  Created by Farooq Rasheed on 27/12/2019.
+//  Copyright © 2019 Farooq Rasheed. All rights reserved.
+//
+
+import XCTest
+
+class SwiftUI_A_Different_TasteUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
